0a26a3776addbb699a7a012a5edd26bb68d802a9
Sowndapan <mvsomupushpa@gmail.com>
2019-07-09 19:56:21 +0530

    work

---
+++ client/build/precache-manifest.1d15954ef1bdaf77c08c6cc06a3569db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41eed1916c90adf8f7efa32c51923b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3231c836eb2f4ddc3105",
+    "url": "/static/css/main.b1f5ec9e.chunk.css"
+  },
+  {
+    "revision": "b1b0257b39a8ed1d7f1c",
+    "url": "/static/js/2.e1f1317c.chunk.js"
+  },
+  {
+    "revision": "3231c836eb2f4ddc3105",
+    "url": "/static/js/main.aa257c3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a27662a7f6120c142de4aeb9d7d5a70",
+    "url": "/static/media/body.1a27662a.jpg"
+  },
+  {
+    "revision": "3903516f1cd402339a219d6f2287be09",
+    "url": "/static/media/landing.3903516f.png"
+  },
+  {
+    "revision": "438eaac65c9934a53719943e3dc7a78f",
+    "url": "/static/media/pokeball.438eaac6.png"
+  },
+  {
+    "revision": "01a254ff90a5603254aa9a361a7dd868",
+    "url": "/static/media/spinner.01a254ff.gif"
+  }
+]);
