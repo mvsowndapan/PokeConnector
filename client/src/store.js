924748dc7fce920c28dbe2ce4b23f70db46cec67
mvsowndapan <mvsomupushpa@gmail.com>
2019-10-09 10:51:37 +0530

    update

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,19 @@
 //redux-store
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 //reducers
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
-//state 
+//state
 const intialState = {};
 //store
 const middleware = [thunk];
 // syntax :: createStore(reducer, initialState, middleware)
-const store = createStore(rootReducer, intialState,
-    compose(applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
-
+const store = createStore(
+  rootReducer,
+  intialState,
+  compose(applyMiddleware(...middleware))
+);
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
